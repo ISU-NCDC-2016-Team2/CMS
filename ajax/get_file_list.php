a30f16de96d2908e1ee4ef707c7c1d643f02a0b3
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-02-05 19:37:37 -0600

    Fixed file displaying

--- ajax/get_file_list.php
+++ ajax/get_file_list.php
@@ -2,21 +2,19 @@
     include_once('../includes/util.php');
     require_authenticated();
 
-	$type = clean_input("/^\.*/", clean_input("/[:\/\\\\\\\\`'\"]/", $_GET["type"]));
+	$type = clean_input("/^\.*/", clean_input("/[:\/\\\\\\\\`'\"\*]/", $_GET["type"]));
 	if ($type != $_GET["type"]) {
 		http_response_code(500);
 		die("error");
 	}
 
-	$dir_name = "E:\\Files\\$type";
+	$dirname = "E:\\Files\\$type";
 
 	if (!file_exists($dirname) || !is_dir($dirname)) {
 		http_response_code(501);
 		die("error1");
 	}
 
-	// TODO -- ACTUAL JSON ANYONE!?
-	// TODO -- Verify user can read directory
 	/*
 	{ "files": [
 		{
@@ -26,12 +24,23 @@
 		}
 	]}
 	*/
+
+    if (!accesschk($_SESSION["username"], $dirname)) {
+		http_response_code(500);
+		die("error2");
+    }
+
 	$files = scandir($dirname);
-	echo "{ \"files\" : [";
+
+    $output = ["files" => []];
 	foreach($files as $file) {
-		if($file == "." or $file == ".." or $file == ".users")
-			continue;
-		echo("\"$file\",");
+		if ($file != "." && $file != ".." && $file != ".users") {
+            if (accesschk($_SESSION["username"], $dirname)) {
+                $obj = ["filename" => $file, "type" => $dirname, "uri" => "/ajax/download.php?type=" . urlencode($dirname) . "&filename=" . urlencode($file)];
+                $output["files"][] = $obj;
+            }
+        }
 	}
-	echo "\"\"]}";
+
+    echo json_encode($output);
 ?>
