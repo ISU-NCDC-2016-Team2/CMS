efbee40b220e86b804d867e8946a9cb8addf3797
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-02-05 20:54:38 -0600

    Working upload?

--- ajax/upload.php
+++ ajax/upload.php
@@ -1,33 +1,42 @@
 <?php
     include_once('../includes/util.php');
-
     require_authenticated();
 
-  $target = "";
-  if (isset($_POST["type"])) {
-    $target = $_POST["type"];
-  }
+	$type = clean_input("/^\.*/", clean_input("/[:\/\\\\\\\\`'\"\*]/", $_POST["type"]));
+	if ($type != $_POST["type"]) {
+		http_response_code(500);
+		die("error");
+	}
 
-  $allowed_extensions = array("jpeg", "jpg", "doc", "odt", "pdf", "txt", "html", "png", "js", "bmp", "xls", "xslx");
+	$dirname = "E:\\Files\\$type";
 
-  if (isset($_FILES['file']) and !empty($target)) {
-    $file = $_FILES['file'];
-    $name = $file['name'];
-    $tmp = $file['tmp_name'];
-    $extension = explode('.', $name)[1];
+	if (!file_exists($dirname) || !is_dir($dirname)) {
+		http_response_code(501);
+		die("error1");
+	}
 
-    if (in_array($extension, $allowed_extensions) === false) {
-      die("Extension not allowed");
-      http_response_code(403);
+    $allowed_extensions = array("jpeg", "jpg", "doc", "odt", "pdf", "txt", "html", "png", "js", "bmp", "xls", "xslx");
+
+    if (isset($_FILES['file'])) {
+        $file = $_FILES['file'];
+        $name = basename(clean_input("/^\.*/", clean_input("/[:\/\\\\\\\\`'\"\*]/", $file['name'])));
+        $extension = substr($name, strrpos($name, '.') + 1);
+        $tmp = $file['tmp_name'];
+
+        if (in_array($extension, $allowed_extensions) === false) {
+            http_response_code(403);
+            die("Extension not allowed");
+        } else {
+            if (!file_exists("$dirname\\$name") && accesschk($_SESSION["username"], $dirname)) {
+                move_uploaded_File($tmp, "$dirname\\$name");
+            }
+
+            http_response_code(200);
+        }
+
     } else {
-      http_response_code(200);
+        http_response_code(400);
+        die("Missing parameters on POST request");
     }
-
-    move_uploaded_File($tmp, "../files/".$target."/".$name);
-
-  } else {
-    http_response_code(400);
-    die("Missing parameters on POST request");
-  }
 
 ?>
