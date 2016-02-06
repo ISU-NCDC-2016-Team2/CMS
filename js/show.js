ebfbce28b7fb52eb4e995c9b68f50b50e1cb7a90
Alexander Scheel <alexander.m.scheel@gmail.com>
2016-02-05 20:05:04 -0600

    Working javascript?

--- js/show.js
+++ js/show.js
@@ -1,26 +1,40 @@
 /* show.js */
 function render_file_list() {
+	$("#files-list").html("");
+
 	var req = new XMLHttpRequest();
-	
-	function cb_add_files() {
-		console.log("Received file list: " + this.responseText);
-		files = JSON.parse(this.responseText).files;
-        console.log("Files: " + files);
-		for (index = 0; index < files.length; index++) {
-            filename = files[index];
-            if (filename != "") {
-			$("#files-list").append('<li><button type="button" class="btn-primary" onclick="download_file(\'' + filename + '\')">Download</button>' + filename + '</li>');
-            }
-		}
+	if (type == "") {
+		req.addEventListener("load", cb_add_types);
+		req.open("GET", "/ajax/get_type_list.php");
+	} else {
+		req.addEventListener("load", cb_add_files);
+		req.open("GET", "/ajax/get_file_list.php?type=" + encodeURIComponent(type));
 	}
-	req.addEventListener("load", cb_add_files);
-	req.open("GET", "/ajax/get_file_list.php?type=" + type);
 	req.send();
 }
 
-function download_file(filename) {
-	$("#after-nav").prepend('<div class="alert alert-success" role="alert">Downloading</div>');
-	$("#after-nav").append('<iframe style="display:none" width="1" height="1" frameborder="0" src="' + "/ajax/download.php?path=" + type + "/" + filename + '"></iframe>');
+function cb_add_types() {
+	console.log("Received type list: " + this.responseText);
+	var folders = JSON.parse(this.responseText).folders;
+	console.log("Folders: " + this.responseText + "\n\n");
+	for (var i = 0; i < folders.length; i++) {
+		var typename = folders[i];
+		if (typename.type != "") {
+			$("#files-list").append('<li><a href="' + typename.uri + '">' + typename.type + '</a></li>');
+		}
+	}
+}
+
+function cb_add_files() {
+	console.log("Received file list: " + this.responseText);
+	var files = JSON.parse(this.responseText).files;
+	console.log("Files: " + this.responseText + "\n\n");
+	for (var i = 0; i < files.length; i++) {
+		var filename = files[index];
+		if (filename.filename != "") {
+			$("#files-list").append('<li><a href="' + filename.uri + '" target="_blank">' + filename.type + "/" + filename.filename + '</a></li>');
+		}
+	}
 }
 
 window.onload = render_file_list();
